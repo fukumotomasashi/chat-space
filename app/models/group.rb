f8791b15a102e6377e0b0467f82218dd9b3b6735
福本雅志 <fukumotomasashi@fukumotomasashinoMacBook.local>
2019-01-05 17:55:11 +0900

    Create message function

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,14 @@
 class Group < ApplicationRecord
   has_many :users, through: :members
-  has_many :messages
+  has_many :massages
   has_many :members
   validates :name, presence: true
+
+  def show_last_message
+    if (last_message = massages.last).present?
+      last_message.boby? ? last_message.boby : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
