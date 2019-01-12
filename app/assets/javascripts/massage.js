6d7bbcc6abf76319072face44a5ec186571ffdf2
fukumotomasashi <cvb25362@gmail.com>
2019-01-12 18:23:01 +0900

    edit massage.js

--- app/assets/javascripts/massage.js
+++ app/assets/javascripts/massage.js
@@ -1,17 +1,54 @@
 $(function (){
-  $('#new_massage').on('submit', function(){
-    var text = $('#massage_boby').val();
-    var image = $('#masage_image').val();
-    var formDate = new FormDate(this);
-    var href = window.location.href
+  function buildHTML(message){
+          var html = (message.image !== null)?
+          `<ul class="content-messages">
+            <li class="content-message">
+              <p>
+                ${ message.name }
+                <span class="content-message__created-time">${ message.created_at }</span>
+              </p>
+              <p class="content-message__text">
+                ${ message.boby }
+              </p>
+              <img class="massage-image" src="${ message.image }" alt="${ message.image }">
+            </li>
+          </ul>`
+          :
+          `<ul class="content-messages">
+            <li class="content-message">
+              <p>
+                ${ message.name }
+                <span class="content-message__created-time">${ message.created_at }</span>
+              </p>
+              <p class="content-message__text">
+                ${ message.boby }
+              </p>`;
+        return html
+    }
+  $('#new_massage').on('submit', function(e){
+    e.preventDefault();
+    $('.content-body').animate({scrollTop: $('.content-body')[0].scrollHeight}, 'fast');
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
 
     $.ajax({
-      url: 'href',
-      type: 'post',
-      date: formDate,
-      dateType: 'json',
-      processDate: false,
-      contentDate: false
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.content-body').append(html);
+      $('#massage_boby').val('');
+      $('#massage_image').val('');
+      $(".message-btn").prop("disabled", false);
+    })
+    .fail(function(){
+      alert('error');
     })
   });
 });
